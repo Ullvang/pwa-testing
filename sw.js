3bbcbde8ca4c0f8d54729ec18568a51a7f5f4438
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-08-24 14:34:00 +0200

    2

--- sw.js
+++ sw.js
@@ -2,8 +2,22 @@ console.log("SW: Registered Event");
 
 self.addEventListener("install", (e) => {
   console.log("SW: Install Event");
+
+  let installPromise = new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+
+  e.waitUntil(installPromise);
+
+  console.log("SW: Install Event Done");
 });
 
 self.addEventListener("activate", (e) => {
   console.log("SW: Activate Event");
+
+  let activatePromise = new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+
+  e.waitUntil(activatePromise);
 });
